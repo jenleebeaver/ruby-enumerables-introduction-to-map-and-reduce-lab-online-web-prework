1196fb081ff608b4044e788fde4ee6a56802da1a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-17 14:59:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,7 +28,7 @@ def reduce_to_total(source_array, starting_point)
   return source_array.reduce(100) {|sum, n| sum + n}
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   new_array = []
   index = 0
   while index < array.size do
