703b7bf31f45dbefc48f85774b3935df58593cc6
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-17 13:59:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
 # My Code here....
+
+def map_to_negativize(array)
+  new_array = array.map { |n| n*-1}
+  return new_array
+end
+
+def map_to_no_change(array)
+  return array
+end
+
+def map_to_double(array)
+  new_array = array.map { |n| n*2}
+  return new_array
+end
+
+def map_to_square(array)
+  new_array = array.map { |n| n**2}
+  return new_array
+end
+
+def reduce_to_total(source_array)
+  return source_array.reduce { |sum, n| sum + n }
+  #return source_array.reduce(100) {|sum, n| sum + n}
+end
+
+def reduce_to_total(source_array, starting_point)
+  return source_array.reduce(100) {|sum, n| sum + n}
+end
+
+def reduce_to_all_true(source_array)
+  return source_array.all? { |word| word }
+end
+
+def reduce_to_any_true(source_array)
+  return source_array.any? { |word| word }
+end
