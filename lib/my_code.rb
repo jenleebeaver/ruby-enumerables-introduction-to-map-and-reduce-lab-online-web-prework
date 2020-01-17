1d204abc2f7d7de72ae1023dbaa167e45a4d8dc5
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-17 15:10:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -28,7 +28,7 @@ end
 
 def reduce_to_total(source_array, starting_point)
   return source_array.reduce() {|sum, n| sum + n}
-  return source_array.reduce(starting_poiny) {|sum, n| sum + n}
+  return source_array.reduce(starting_point) {|sum, n| sum + n}
 end
 
 #def reduce_to_total(source_array, starting_point = 0)
